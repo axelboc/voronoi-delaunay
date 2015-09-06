665571bb9ee15a00000343151d0a21dabbf7884b
Axel Bocciarelli <axelboc@gmail.com>
2015-09-06 11:24:39 +1000

    Edge test suite

---
+++ test/edge.js
@@ -0,0 +1,20 @@
+
+import {expect} from 'chai';
+import Vertex from '../app/lib/vertex';
+import Edge from '../app/lib/edge';
+
+describe('Edge', function () {
+
+	describe('constructor', function () {
+
+		it('should accept two vertices', function () {
+			const v1 = new Vertex(0, 0), v2 = new Vertex(1, 1);
+			const edge = new Edge(v1, v2);
+
+			expect(edge.v1).to.equal(v1);
+			expect(edge.v2).to.equal(v2);
+		});
+
+	});
+
+});
--- test/vertex.js
+++ test/vertex.js
@@ -2,17 +2,18 @@
 import {expect} from 'chai';
 import Vertex from '../app/lib/vertex';
 
-describe('vertex', function () {
+describe('Vertex', function () {
 
 	describe('constructor', function () {
 
-		it('should have `x` and `y` properties', function () {
+		it('should accept `x` and `y` coordinates', function () {
 			const x = 1, y = 2;
-			const v = new Vertex(x, y);
+			const vertex = new Vertex(x, y);
 
-			expect(v.x).to.equal(x);
-			expect(v.y).to.equal(y);
+			expect(vertex.x).to.equal(x);
+			expect(vertex.y).to.equal(y);
 		});
 
 	});
+	
 });
