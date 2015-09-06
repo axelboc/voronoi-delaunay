5fd73c7ec2eda95bc139f08cc679e77662008eef
Axel Bocciarelli <axelboc@gmail.com>
2015-09-06 11:42:42 +1000

    add assertion helpers

--- app/lib/assert.js
+++ app/lib/assert.js
@@ -9,3 +9,11 @@ export default function assert(condition, message) {
 		throw new Error("Assertion Error" + (message ? ": " + message : ""));
 	}
 }
+
+assert.integerGt0 = function (val) {
+	assert(typeof val === 'number' && val > 0 && val % 1 === 0, "must be integer greater than zero");
+};
+
+assert.nonEmptyString = function (str) {
+	assert(typeof str === 'string' && str.length > 0, "must be non-empty string");
+};
